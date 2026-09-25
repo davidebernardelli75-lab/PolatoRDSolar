28de88ec195e10c105ca0536f36480c83cf1a231
davidebernardelli75-lab <davidebernardelli75@gmail.com>
2026-09-25 09:50:21 +0200

    Populate Huawei inverter and battery models

--- src/lib/equipment-presets.ts
+++ src/lib/equipment-presets.ts
@@ -1,11 +1,31 @@
 export const INVERTER_BRANDS = ['HUAWEI'] as const;
 
 export const INVERTER_MODELS: Record<string, readonly string[]> = {
-  HUAWEI: [],
+  HUAWEI: [
+    'SUN2000-3KTL-L1',
+    'SUN2000-4KTL-L1',
+    'SUN2000-5KTL-L1',
+    'SUN2000-6KTL-L1',
+    'SUN2000-8KTL-L1',
+    'SUN2000-10KTL-L1',
+    'SUN2000-12KTL-L1',
+    'SUN2000-3KTL-M1',
+    'SUN2000-4KTL-M1',
+    'SUN2000-5KTL-M1',
+    'SUN2000-6KTL-M1',
+    'SUN2000-8KTL-M1',
+    'SUN2000-10KTL-M1',
+    'SUN2000-12KTL-M1',
+  ],
 };
 
 export const STORAGE_BRANDS = ['HUAWEI'] as const;
 
 export const STORAGE_MODELS: Record<string, readonly string[]> = {
-  HUAWEI: [],
+  HUAWEI: [
+    'LUNA2000-5KW',
+    'LUNA2000-7KW',
+    'LUNA2000-10KW',
+    'LUNA2000-15KW',
+  ],
 };
